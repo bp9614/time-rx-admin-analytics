7ff0b1ff82e5a865bca20e7745d60dedf2303ee2
bp9614 <bpham9614@gmail.com>
2018-07-20 09:06:29 -0400

    Base Index JSX and CSS

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { createStore, applyMiddleware} from 'redux';
+import ReduxPromise from 'redux-promise';
+
+import AnalyticsPage from './components/analytics_page';
+import LoginPage from './components/login_page';
+import reducers from './reducers';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/analytics" component={AnalyticsPage}/>
+                    <Route path="/" component={LoginPage}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
+
+registerServiceWorker();
