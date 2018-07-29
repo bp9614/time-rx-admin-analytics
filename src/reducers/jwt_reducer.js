1f2e452bd6913bea58f7523530235fea0aeb2bba
bp9614 <bpham9614@gmail.com>
2018-07-29 11:53:04 -0400

    Removed loading state for now and added AUTHENICATED case and default state

--- src/reducers/jwt_reducer.js
+++ src/reducers/jwt_reducer.js
@@ -2,16 +2,39 @@ import _ from 'lodash';
 
 import * as actionTypes from '../actions/action_types';
 
-export default function (state = {}, action) {
+export default function (state = {authenticated: false}, action) {
   switch (action.type) {
-    case actionTypes.FETCH_JWT_SUCCESS:
-      state = { ...state, ...action.payload.data, is_available: true };
+    case actionTypes.AUTHENICATED:
+      state = {
+        ...state,
+        authenticated: true,
+      }
       break;
-    case actionTypes.REFRESH_JWT_SUCCESS:
-      state = { ...state, ...action.payload.data, is_available: true };
+    case actionTypes.FETCH_JWT_SUCCESS:
+      state = {
+        ...state, 
+        ...action.payload.data, 
+        authenticated: true
+      };
+      break;
+    case actionTypes.VERIFY_JWT_SUCCESS:
+      state = {
+        ...state, 
+        ...action.payload.data, 
+        authenticated: true
+      };
+      break;
+    case actionTypes.VERIFY_JWT_FAILURE:
+      state = {
+        ...state, 
+        authenticated: false
+      };
       break;
     case actionTypes.LOGOUT:
-      state = _.omit({ ...state, is_available: true }, ['access', 'refresh']);
+      state = _.omit({ ...state, authenticated: true },
+        ['access', 'refresh']);
+      break;
+    default:
       break;
   }
 
