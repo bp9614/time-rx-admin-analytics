22bc3b3b97e8657499c4dcb344c1acad7cd21338
bp9614 <bpham9614@gmail.com>
2018-07-29 11:51:18 -0400

    Fixed token names and combind refreshJWT and verifyJWT

--- src/actions/jwt.js
+++ src/actions/jwt.js
@@ -3,17 +3,19 @@ import axios from 'axios';
 import * as consts from '../consts';
 import * as actionTypes from './action_types';
 
-export function fetchJWT(username, password) {
+export function fetchJWT(username, password, callback) {
   const response = axios.post(consts.DJANGO_URL + 'api/token/',
-    {username, password} , consts.AXIOS_HEADERS);
+      {username, password} , consts.AXIOS_HEADERS);
 
   return (dispatch) => {
     response
       .then(({data}) => {
-        sessionStorage.setItem('', response.data.access);
-        sessionStorage.setItem('', response.data.refresh);
+        sessionStorage.setItem('admin_analytics_access', data.access);
+        sessionStorage.setItem('admin_analytics_refresh', data.refresh);
 
         dispatch({type: actionTypes.FETCH_JWT_SUCCESS, payload: data});
+        
+        callback();
       })
       .catch((error) => {
         dispatch({type: actionTypes.FETCH_JWT_FAILURE});
@@ -22,34 +24,44 @@ export function fetchJWT(username, password) {
 }
 
 export function logout() {
-    sessionStorage.removeItem('admin_analytics_token');
-    sessionStorage.removeItem('admin_analytics_refresh_token');
+    sessionStorage.removeItem('admin_analytics_access');
+    sessionStorage.removeItem('admin_analytics_refresh');
 
     return {
         type: actionTypes.LOGOUT,
     };
 }
 
-export function refreshJWT(refreshToken) {
-  const response = axios.post(consts.DJANGO_URL + 'api/token/refresh/', 
-    {refresh: refreshToken}, consts.AXIOS_HEADERS);
+// export function refreshJWT(refreshToken) {
+//   const response = axios.post(consts.DJANGO_URL + 'api/token/refresh/', 
+//       {refresh: refreshToken}, consts.AXIOS_HEADERS);
 
-  return (dispatch) => {
-    response
-      .then(({data}) => {
-        sessionStorage.setItem('', response.data.access);
+//   return (dispatch) => {
+//     response
+//       .then(({data}) => {
+//         sessionStorage.setItem('admin_analytics_access', response.data.access);
                 
-          dispatch({type: actionTypes.REFRESH_JWT_SUCCESS, payload: data});
-      })
-      .catch((error) => {
-        dispatch({type: actionTypes.REFRESH_JWT_FAILURE});
-      });
-  };
-}
+//         dispatch({type: actionTypes.REFRESH_JWT_SUCCESS, payload: data});
+//       })
+//       .catch((error) => {
+//         dispatch({type: actionTypes.REFRESH_JWT_FAILURE});
+//       });
+//   };
+// }
 
-export function verifyJWT(accessToken) {
+function refreshJWT(refreshToken){
+  const response = axios.post(consts.DJANGO_URL + 'api/token/refresh/', 
+      {refresh: refreshToken}, consts.AXIOS_HEADERS)
+    .then(({data}) => {
+      return data.access;
+    })
+    .error((error) => {
+      return null;
+    });
+}
+export function verifyJWT(accessToken, refreshToken) {
   const response = axios.post(consts.DJANGO_URL + 'api/token/verify/',
-    {access: accessToken}, consts.AXIOS_HEADERS);
+      {token: accessToken}, consts.AXIOS_HEADERS);
 
   return (dispatch) => {
     response
@@ -57,7 +69,13 @@ export function verifyJWT(accessToken) {
         dispatch({type: actionTypes.VERIFY_JWT_SUCCESS});
       })
       .catch((error) => {
-        dispatch({type: actionTypes.VERIFY_JWT_FAILURE});
+        const newAccessToken = refreshJWT(refreshToken);
+        if (newAccessToken) {
+          sessionStorage.setItem('admin_analytics_access' , newAccessToken);
+          dispatch({type: actionTypes.VERIFY_JWT_SUCCESS});
+        } else {
+          dispatch({type: actionTypes.VERIFY_JWT_FAILURE});
+        }
       });
   };
 }
