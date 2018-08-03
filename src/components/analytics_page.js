6704cd099961413e972eb60fac4cb9bf72e49da4
bp9614 <bpham9614@gmail.com>
2018-08-02 23:32:58 -0400

    Readying to verify JWTs

--- src/components/analytics_page.js
+++ src/components/analytics_page.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
-export default class AnalyticsPage extends Component {
+import { connect } from 'react-redux';
+
+import { verifyJWT } from '../actions/jwt';
+
+class AnalyticsPage extends Component {
   render() {
     return (
       <div>Analytics Page!</div>
     );
   }
 }
+
+export default connect(null, { verifyJWT })(AnalyticsPage);
