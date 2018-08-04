b9a97c3860df9448da703dd0380b7abd6750d421
bp9614 <bpham9614@gmail.com>
2018-08-04 09:27:34 -0400

    Changed URL of API anf added account for checking authentication

--- timerx/timerx/urls.py
+++ timerx/timerx/urls.py
@@ -6,12 +6,13 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('analytics.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    re_path('^api/', include('analytics.urls')),
     re_path(r'^api/token/$', TokenObtainPairView.as_view(),
             name='token_obtain_pair'),
     re_path(r'^api/token/refresh/$', TokenRefreshView.as_view(),
             name='token_refresh'),
     re_path(r'^api/token/verify/$', TokenVerifyView.as_view(),
-            name='token_verify')
+            name='token_verify'),
 ]
