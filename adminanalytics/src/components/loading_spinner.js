c0940e96c152b0c9973fff6902439b7df597886e
bp9614 <bpham9614@gmail.com>
2018-08-05 19:13:57 -0400

    Finished loading spinner

--- adminanalytics/src/components/loading_spinner.js
+++ adminanalytics/src/components/loading_spinner.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Loader, Modal } from 'semantic-ui-react';
 
-export default class LoadingSpinner extends Component{
+class LoadingSpinner extends Component{
   render() {
     return (
-      <div>Imagine a Loading Page</div>
+      <Modal basic open={this.props.isLoading} size="mini">
+        <Modal.Content>
+          <Loader size="large" active>Loading</Loader>
+        </Modal.Content>
+      </Modal>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    isLoading: state.loading.isLoading,
+  }
+}
+
+export default connect(mapStateToProps)(LoadingSpinner);
