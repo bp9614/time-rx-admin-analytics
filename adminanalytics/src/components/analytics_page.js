b3add761104db67dd7a2a4a11dcbeb1812fdc399
bp9614 <bpham9614@gmail.com>
2018-08-05 14:59:35 -0400

    Added menu bar

--- adminanalytics/src/components/analytics_page.js
+++ adminanalytics/src/components/analytics_page.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
-
-import { verifyJWT } from '../actions/jwt';
+import { Loader, Menu } from 'semantic-ui-react';
+import { getUsername, logout, verifyFirst } from '../actions/jwt';
 
 class AnalyticsPage extends Component {
+  componentWillMount() {
+    if (!this.props.username) {
+      this.props.verifyFirst(
+        this.props.access, 
+        this.props.refresh, 
+        getUsername,
+      );
+    }
+  }
+
   render() {
     return (
-      <div>Analytics Page!</div>
+      <div>
+        <Menu inverted color="blue" borderless>
+          <Menu.Item disabled={true}>
+            <p className="cursive-font larger-text">Time-Rx</p>
+          </Menu.Item>
+          <Menu.Item>
+            {this.props.username ? 
+              <p className="cursive-font larger-text">
+                Welcome, {this.props.username}
+              </p> : <Loader active></Loader>}
+          </Menu.Item>
+          <Menu.Item
+              position="right"
+              onClick={this.props.logout}>
+            <p className="larger-text">Logout</p>
+          </Menu.Item>
+        </Menu>
+      </div>
     );
   }
 }
 
-export default connect(null, { verifyJWT })(AnalyticsPage);
+function mapStateToProps(state) {
+  return {
+    access: state.jwt.access,
+    refresh: state.jwt.refresh,
+    username: state.jwt.username,
+  }
+}
+
+export default connect(mapStateToProps, {
+  logout, 
+  verifyFirst, 
+})(AnalyticsPage)
