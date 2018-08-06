2d77517ce4e8a070347432a7c97d321b6819a108
bp9614 <bpham9614@gmail.com>
2018-08-05 20:35:25 -0400

    Added possible collections

--- adminanalytics/src/consts/index.js
+++ adminanalytics/src/consts/index.js
@@ -1,5 +1,11 @@
-export const LOGIN_ERROR_MESSAGE = 'INCORRECT USERNAME OR PASSWORD';
+export const COLLECTIONS = [
+  'CloudTrail',
+  'Cognito',
+  'DynamoDB',
+  'History',
+];
 export const DJANGO_URL = 'http://localhost:8000/';
+export const LOGIN_ERROR_MESSAGE = 'INCORRECT USERNAME OR PASSWORD';
 export const AXIOS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': [
